51edcf6d1b79cfa89e8fb4161e21d61490b4c980
qkqkwl159 <qkqkwl147@gmail.com>
2022-05-09 17:08:03 +0900

    LOG

--- log.h
+++ log.h
@@ -1,10 +1,5 @@
-#ifdef __LOG_H__
-#define __LOG_H__
-
 #ifdef DEBUG
-#define LOG(...) do{printf("LINE : %d FUNC : %s ", __LINE__, __func__);printf(_VA_ARGS__);}while(0)
+#define LOG(...) do{ printf("LINE: %d FUNC: %s ", __LINE__, __func__); printf("\n"); printf(__VA_ARGS__); }while(0);
 #else
 #define LOG(X)
-#endif
-
 #endif
